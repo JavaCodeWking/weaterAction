0c532e83bed1b187a96b5e02a5a0bcf75ec5ffe0
JavaCodeWking <1365413148@qq.com>
2022-02-26 23:35:54 +0800

    update

--- weater.py
+++ weater.py
@@ -4,27 +4,37 @@
 import requests, json
 import os
 
-SCKEY=os.environ.get('SCKEY') ##Server酱推送KEY
-
-def ServerPush(info): #Server酱推送
+SCKEY=os.environ.get('SCKEY')
+SKEY=os.environ.get('SKEY')
+def MLMPush(info):
     api = " https://sctapi.ftqq.com/{}.send".format(SCKEY)
-    title = "天气推送"
+    title = "该打卡了，晴晴"
     content = info.replace('\n','\n\n')
     data = {
         "text": title,
         "desp": content
     }
-    print(content)
     requests.post(api, data=data)
+
+def WQPush(info):
+    api = " https://sctapi.ftqq.com/{}.send".format(SKEY)
+    title = "该打卡了，晴晴"
+    content = info.replace('\n','\n\n')
+    data = {
+        "text": title,
+        "desp": content
+    }
+    requests.post(api, data=data)
+
 def main():
     try:
-        tdwt = "你哈珀"
-        ServerPush(tdwt)
+        text = "爱你晴晴，记得打卡呀"
+        WQPush(text)
+        MLMPush(text)
     except Exception:
-        error = '【出现错误】\n　　今日天气推送错误，请检查服务或网络状态！'
+        error = '【出现错误】\n　　今日推送错误，请检查服务或网络状态！'
         print(error)
         print(Exception)
 
 if __name__ == '__main__':
     main()
-    
